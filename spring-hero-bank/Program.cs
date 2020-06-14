8075dbf3028d9ff4272bf225128bdff8de8aa729
namdinh247 <40781287+nguyenvanhientnt@users.noreply.github.com>
2020-06-14 17:16:47 +0700

    thử lại

--- spring-hero-bank/Program.cs
+++ spring-hero-bank/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace spring_hero_bank
+﻿using spring_hero_bank.View;
+
+namespace spring_hero_bank
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            GeneratorMenu.GenerateMenu();
         }
     }
 }
--- spring-hero-bank/View/GeneratorMenu.cs
+++ spring-hero-bank/View/GeneratorMenu.cs
@@ -1,7 +1,42 @@
+using System;
+using spring_hero_bank.Controller;
+
 namespace spring_hero_bank.View
 {
     public class GeneratorMenu
     {
-        
+        public static void GenerateMenu()
+        {
+            var controller = new AccountController();
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("—— Ngân hàng Spring Hero Bank ——");
+                Console.WriteLine("1. Đăng ký tài khoản.");
+                Console.WriteLine("2. Đăng nhập hệ thống.");
+                Console.WriteLine("3. Thoát.");
+                Console.WriteLine("——————————————————-");
+                Console.WriteLine("Nhập lựa chọn của bạn (1,2,3): ");
+                var choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        controller.Register();
+                        break;
+                    case 2:
+                        controller.Login();
+                        break;
+                    case 3:
+                        Console.WriteLine("Cảm ơn quý khách đã sử dụng dịch vụ của chúng tôi!");
+                        break;
+                }
+
+                Console.ReadLine();
+                if (choice == 3)
+                {
+                    break;
+                }
+            }
+        }
     }
 }
